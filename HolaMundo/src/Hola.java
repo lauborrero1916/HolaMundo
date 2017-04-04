c20ee7b341343fb37dcb5bb280c07cf954fbbba1
Laura Borrero Romero <Laura Borrero Romero@LauraBorrero.home>
2017-04-04 11:08:18 -0500

    cambio inicial 1

---
+++ HolaMundo/src/Hola.java
@@ -0,0 +1,14 @@
+
+public class Hola {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+						
+		System.out.println("HOLA MUNDO ");
+		System.out.println("Soy laura y este es un ejemplo");
+		
+
+	}
+
+}
